94735875614460bfb6ede3a591d72292f6c401a2
pchizhov <chizhov.pasha@yandex.ru>
2022-05-09 13:11:43 +0300

    simple commands

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import threading
 from enum import Enum
 
 
+MAX_PID = 1
+
+
 class State(Enum):
     NF = 0,
     F = 1
@@ -40,17 +43,36 @@ if __name__ == '__main__':
         print('Number of processes should be positive integer.')
         exit(1)
     processes = init_processes(N)
+    MAX_PID = N + 1
     for p in processes:
         _thread.start_new_thread(p.start, ())
     while True:
         command = input('$ ').split(' ')
-        if command[0] == 'g-state':
-            if len(command) == 1:
-                p_list = [f'G{p.pid}, {p.role.name.lower()}, state={p.state.name}' for p in processes]
-                print(*p_list, sep='\n')
-            else:
+        if command[0] == 'actual-order':
+            order = command[1]
+        elif command[0] == 'g-state':
+            if len(command) > 1:
                 pid = int(command[1])
                 state = State.NF if command[1] == 'non-faulty' else State.F
+                for p in processes:
+                    if p.pid == pid:
+                        p.state = state
+            else:
+                p_list = [f'G{p.pid}, {p.role.name.lower()}, state={p.state.name}' for p in processes]
+                print(*p_list, sep='\n')
+        elif command[0] == 'g-kill':
+            pid = int(command[1])
+            for i, p in enumerate(processes):
+                if p.pid == pid:
+                    processes.pop(i)
+                    if p.role == Role.PRIMARY:
+                        processes[0].role = Role.PRIMARY
+        elif command[0] == 'g-add':
+            n = int(command[1])
+            for i in range(n):
+                processes.append(Process(MAX_PID, Role.SECONDARY))
+                _thread.start_new_thread(processes[-1].start, ())
+                MAX_PID += 1
         elif command[0] in ['exit', 'quit']:
             break
         else:
